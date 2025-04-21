e1e6d163299d91963cd7de3508e874f4c2243a84
alin-comcity <1000957@daffodil.ac>
2025-04-21 18:53:17 +0600

    try to make more dynamic with json data

--- filterproduct.js
+++ filterproduct.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
       filterButtons.forEach((btn) => {
         btn.addEventListener("click", () => {
+          
           const filter = btn.getAttribute("data-filter");
 
           // Update button styles
@@ -25,5 +26,40 @@ document.addEventListener('DOMContentLoaded', function() {
           });
         });
       });
-
+  
 });
+
+
+document.addEventListener('alpine:init', () => {
+  Alpine.data('app', () => ({
+    categories: [],
+    galleryItems: [],
+      async readData() {
+          this.categories = await getCategories();
+          this.galleryItems = await getGalleryItems();
+      }
+  }))      
+});
+
+let categories = [
+  {"id":1, "name":"All", "data-filter":"all"},
+  {"id":1, "name":"Web Design" , "data-filter":"web-design"},
+  {"id":1, "name":"App Development" , "data-filter":"app-development"},
+  {"id":1, "name":"Branding", "data-filter":"branding"}
+];
+
+let galleryItems = [
+  {"id":1, "name":"web-design", "data-filter":"all", "url":"https://images.unsplash.com/photo-1583508915901-b5f84c1dcde1?q=80&w=1024&auto=format&fit=crop", "thumb":"https://images.unsplash.com/photo-1583508915901-b5f84c1dcde1?q=80&w=400&h=300&auto=format&fit=crop"},
+  {"id":1, "name":"web-design", "data-filter":"web-design", "url":"https://images.unsplash.com/photo-1583508915901-b5f84c1dcde1?q=80&w=1024&auto=format&fit=crop", "thumb":"https://images.unsplash.com/photo-1583508915901-b5f84c1dcde1?q=80&w=400&h=300&auto=format&fit=crop"},
+  {"id":1, "name":"app-development", "data-filter":"app-development", "url":"https://images.unsplash.com/photo-1583508915901-b5f84c1dcde1?q=80&w=1024&auto=format&fit=crop", "thumb":"https://images.unsplash.com/photo-1583508915901-b5f84c1dcde1?q=80&w=400&h=300&auto=format&fit=crop"},
+  {"id":1, "name":"branding", "data-filter":"branding", "url":"https://images.unsplash.com/photo-1583508915901-b5f84c1dcde1?q=80&w=1024&auto=format&fit=crop", "thumb":"https://images.unsplash.com/photo-1583508915901-b5f84c1dcde1?q=80&w=400&h=300&auto=format&fit=crop"},
+  {"id":1, "name":"web-design", "data-filter":"web-design", "url":"https://images.unsplash.com/photo-1583508915901-b5f84c1dcde1?q=80&w=1024&auto=format&fit=crop", "thumb":"https://images.unsplash.com/photo-1583508915901-b5f84c1dcde1?q=80&w=400&h=300&auto=format&fit=crop"},
+];
+
+async function getCategories() {
+  return categories;
+}
+
+async function getGalleryItems() {
+  return galleryItems;
+}
